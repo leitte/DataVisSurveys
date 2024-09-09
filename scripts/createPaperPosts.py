62c1d00d3a63800b527d6cfb64250d59c44f9a6a
Heike Leitte <leitte@rptu.de>
2024-09-09 13:00:28 +0200

    update

--- scripts/createPaperPosts.py
+++ scripts/createPaperPosts.py
@@ -1,9 +1,21 @@
 import os
+import json
+import yaml
 
 directory = 'data/paperMetadata'
+
+def loadPaperMetadata (filepath):
+    with open(filepath, 'r') as file:
+        paperMetadata = json.load(file)
+        return paperMetadata
 
 for filename in os.listdir(directory):
     filepath = os.path.join(directory, filename)
 
     if os.path.isfile(filepath):
         print('++',filepath)
+        paperMetadata = loadPaperMetadata(filepath)
+        if paperMetadata:
+            print(paperMetadata['title'])
+
+    break
